8d15c234da28c2d1d2fbe1a2f47e5d40d67e92ae
tanvir <tanvirulislam149@gmail.com>
2022-07-17 22:27:51 +0600

    sidebar done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-      "./src/**/*.{js,jsx,ts,tsx}",
-      "./src/**/*.{html,js}",
-      "./node_modules/tw-elements/dist/js/**/*.js",
-   ],
-   content: [],
+   content: ["./src/**/*.{js,jsx,ts,tsx}"],
    theme: {
       extend: {},
    },
-   plugins: [require("tw-elements/dist/plugin")],
+   plugins: [],
 };
